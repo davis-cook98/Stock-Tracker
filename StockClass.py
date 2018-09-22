1e4a226fea3d0585710f7148cf7a436458b14940
Davis Cook <daviscook@outlook.com>
2018-09-21 21:08:25 -0400

    Commented the Stock class

--- StockClass.py
+++ StockClass.py
@@ -5,19 +5,24 @@
 #More to come but this is the start
 
 class Stock:
+    #Defines the fields for a Stock
     def _init_(self, Name, Symbol, Price):
         self._Name = Name
         self._Symbol = Symbol
         self._Price = Price
 
+    #Returns the stock's name
     def getName(self):
         return self._name
 
+    #Returns the stock's symbol
     def getSymbol(self):
         return self._Symbol
 
+    #Returns the stock's price
     def getPrice(self):
         return self._Price
 
+    #Prints the stock's name followed by the price
     def Ticker(self):
         print(Name + "'s current price is: " + str(self._price))
