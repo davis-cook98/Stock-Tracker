c052004a63b6611fb03bccbb1c98ea7756d337f3
Davis Cook <daviscook@outlook.com>
2019-01-18 15:09:18 -0500

    Fixed StockClass and added Driver

---
+++ Driver.py
@@ -0,0 +1,12 @@
+#https://gspread.readthedocs.io/en/latest/
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+#used to communicate with Google Sheets
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+sheet = client.open("Stock-Tracker Backend").sheet1
+#Row, Column for writing
--- StockClass.py
+++ StockClass.py
@@ -6,16 +6,14 @@
 #A Price which is a long
 
 class Stock:
-    #Defines the fields for a Stock
-    def __init__(self, Name, Symbol, Price, nChange):
-        self.name = Name
-        self.symbol = Symbol
-        self.price = Price
-        self.change = nChange
+    def __init__(self, Name, Symbol, Price):
+        self.Name = Name
+        self.Symbol = Symbol
+        self.Price = Price
 
-    def __str__(self):
-        return ', '.join(['{key}={value}'.format(key=key, value=self.__dict__.get(key)) for key in self.__dict__]
 
-X = Stock(Name="Apple", Symbol="AAPL", Price=203.77 nChange=1.03)
+X = Stock("Apple", "AAPL", 203.77)
 
-print(X)
+print(X.Name)
+print(X.Symbol)
+print(X.Price)
